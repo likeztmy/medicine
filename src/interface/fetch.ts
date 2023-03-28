7f60699537b8ffff95bdfa52d4ce7413369c4e23
likeztmy <413713942@qq.com>
2023-03-28 22:47:25 +0800

    fix

--- src/interface/fetch.ts
+++ src/interface/fetch.ts
@@ -1,5 +1,5 @@
 export async function getJson(page:number,type:string) {
-    const res = await fetch(`http://139.196.30.123:80/api/v1/info?page=${page}&medicine_type=${type}`,{
+    const res = await fetch(`http://139.196.30.123:8080/api/v1/info?page=${page}&medicine_type=${type}`,{
         method: 'GET',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
@@ -14,7 +14,7 @@ export async function getSearch(keyWord:string,page:number) {
     const formdata = new FormData()
     formdata.append('text',keyWord)
     formdata.append('page',`${page}`)
-    const res = await fetch('http://139.196.30.123:80/api/v1/info/search',{
+    const res = await fetch('http://139.196.30.123:8080/api/v1/info/search',{
         method: 'POST',
         headers: {
             // 'Content-Type': 'multipart/form-data;charset=utf-8' 
@@ -25,7 +25,7 @@ export async function getSearch(keyWord:string,page:number) {
 }
 
 export async function getData(type:string) {
-    const res = await fetch(`http://139.196.30.123:80/api/v1/info/${type}`,{
+    const res = await fetch(`http://139.196.30.123:8080/api/v1/info/${type}`,{
         method:'GET',
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
